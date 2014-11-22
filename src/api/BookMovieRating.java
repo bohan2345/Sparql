9b21aceecd69853d2a7d10c4f383b66e5c91145b
bohan2345 <bohan.zheng@gmail.com>
2014-11-22 15:36:36 -0800

    update

---
+++ src/api/BookMovieRating.java
@@ -0,0 +1,48 @@
+package api;
+
+import static api.CrawlData.SOURCE;
+import static api.CrawlData.prefix;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+
+import com.hp.hpl.jena.ontology.OntModel;
+import com.hp.hpl.jena.ontology.OntModelSpec;
+import com.hp.hpl.jena.query.Query;
+import com.hp.hpl.jena.query.QueryExecution;
+import com.hp.hpl.jena.query.QueryExecutionFactory;
+import com.hp.hpl.jena.query.QueryFactory;
+import com.hp.hpl.jena.query.ResultSet;
+import com.hp.hpl.jena.query.ResultSetFormatter;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+
+public class BookMovieRating {
+	public void get(String path) {
+
+		String sparql = "SELECT ?book ?brating ?movie ?mrating ?author " +
+				"WHERE{" +
+				"?author hw:wrote ?book ." +
+				"?book hw:filmed ?movie ." +
+				"?book hw:bookrating ?brating ." +
+				"?movie hw:imdbrating ?mrating ." +
+				"FILTER (?brating >=3.5 && ?mrating >=7.5)" +
+				"} " +
+				"ORDER BY DESC(?mrating)";
+
+		OntModel model1 = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM_MICRO_RULE_INF);
+		model1.read(SOURCE, "RDF/XML");
+		Query query = QueryFactory.create(prefix + sparql);
+
+		QueryExecution qexec = QueryExecutionFactory.create(query, model1);
+
+		ResultSet results = qexec.execSelect();
+		try {
+			OutputStream output = new FileOutputStream(path + "test2.json");
+			ResultSetFormatter.outputAsJSON(output, results);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
